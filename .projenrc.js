2db2c1cc66469e6e47ced3eef6ee229a985bb224
Blake Green <blakegreen@msn.com>
2021-12-28 14:47:31 -0500

    initial commit v10

--- .projenrc.js
+++ .projenrc.js
@@ -5,11 +5,21 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   name: "projen-party",
 
   // cdkDependencies: undefined,  /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
-  deps: ["@aws-sdk/client-dynamodb", "dynamodb-onetable"],
+  deps: [
+    "@aws-sdk/client-dynamodb",
+    "dynamodb-onetable",
+    "@aws-cdk/aws-apigatewayv2-alpha",
+    "@aws-cdk/aws-apigatewayv2-integrations-alpha",
+    "react",
+    "react-dom",
+  ],
   // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: [
     "@types/aws-lambda",
     "esbuild",
+    "@types/react-dom",
+    "@vitejs/plugin-react-refresh",
+    "vite",
   ] /* Build dependencies for this module. */,
   // packageName: undefined,      /* The "name" in package.json. */
   // release: undefined,          /* Add release management to this project. */
--- src/lib/webapp-stack.ts
+++ src/lib/webapp-stack.ts
@@ -1,12 +1,17 @@
-import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import { CorsHttpMethod, HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
 import { BillingMode, AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Architecture } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
 
 export class WebAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Table(this, 'Table', {
+    const table = new Table(this, 'Table', {
       partitionKey: { name: 'pk', type: AttributeType.STRING},
       sortKey: { name: 'sk', type: AttributeType.STRING},
       billingMode: BillingMode.PAY_PER_REQUEST,
@@ -14,6 +19,44 @@ export class WebAppStack extends Stack {
       tableName: 'NotesTable'
     })
 
+    const readFunction = new NodejsFunction(this, "ReadNotesFn", {
+      architecture: Architecture.ARM_64,
+      entry: `${__dirname}/fns/readFunction.ts`,
+      logRetention: RetentionDays.ONE_WEEK
+    })
 
+    const writeFunction = new NodejsFunction(this, "WriteNotesFn", {
+      architecture: Architecture.ARM_64,
+      entry: `${__dirname}/fns/writeFunction.ts`,
+      logRetention: RetentionDays.ONE_WEEK
+    })
+
+    table.grantReadData(readFunction);
+    table.grantWriteData(writeFunction);
+
+    const api = new HttpApi(this, 'NotesApi', {
+      corsPreflight: {
+        allowHeaders: ['Content-Type'],
+        allowMethods: [CorsHttpMethod.GET, CorsHttpMethod.POST],
+        allowOrigins: ['*']
+      }
+    })
+
+    const readIntegration = new HttpLambdaIntegration('ReadIntegration', readFunction)
+    const writeIntegration = new HttpLambdaIntegration('WriteIntegration', writeFunction)
+
+    api.addRoutes({
+      integration: readIntegration,
+      methods: [HttpMethod.GET],
+      path: '/notes'
+    })
+
+    api.addRoutes({
+      integration: writeIntegration,
+      methods: [HttpMethod.POST],
+      path: '/notes'
+    })
+
+    new CfnOutput(this, 'HttpApiUrl', {value: api.apiEndpoint})
   }
 }
